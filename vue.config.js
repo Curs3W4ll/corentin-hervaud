8bacf8887068a0b74c322aa6bdf73d2cf4fabf46
Corentin Hervaud <corentin.hervaud@epitech.eu>
2022-05-16 22:34:36 +0200

    Setup

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "production" ? "/corentin-hervaud/" : "/",
-}
+  transpileDependencies: true,
+})
