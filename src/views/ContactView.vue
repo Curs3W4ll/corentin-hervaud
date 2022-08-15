8a2cbc89d64a7a94ed48944ca675052b3abc9a6b
Corentin Hervaud <corentin.hervaud@epitech.eu>
2022-08-15 16:55:47 +0200

    feat(main): Create contact page #20

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,3 +1,75 @@
 <template>
-  <h1>Contact</h1>
+  <AnimatedBackground />
+  <div id="mainContactsContainer">
+    <div class="contactColumn" v-for="column in columns" :key="column.id">
+      <ContactItem class="contactItem" v-for="item in column.items" :key="item.id" :title="item.name" :value="item.value" :link="item.link" />
+    </div>
+  </div>
 </template>
+
+<script>
+import sourceData from "@/data.json";
+
+import ContactItem from "@/components/ContactItem.vue";
+import AnimatedBackground from "@/components/AnimatedBackground.vue";
+
+export default {
+  computed: {
+    columns() {
+      const columnNumber = 2;
+      let columns = [];
+
+      for (let colId = 0; colId < columnNumber; colId++) {
+        columns.push({ id: colId, items: [] });
+      }
+
+      let colId = 0;
+      for (let contact of sourceData.contacts) {
+        contact.id = columns[colId].items.length;
+        columns[colId].items.push(contact);
+        colId++;
+        if (colId >= columnNumber) colId = 0;
+      }
+
+      return columns;
+    },
+  },
+  components: {
+    ContactItem,
+    AnimatedBackground,
+  },
+};
+</script>
+
+<style scoped>
+#mainContactsContainer {
+  display: flex;
+  flex: 1;
+  margin: 10% 15% 0 15%;
+}
+.contactColumn {
+  margin: 3%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+.contactItem {
+  margin-bottom: 20%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+}
+.table {
+  padding: 0 10% 0 10%;
+  width: 100%;
+}
+.tableLine {
+  display: flex;
+  flex: 1;
+}
+.tableCase {
+  margin: 50px;
+  display: flex;
+  flex: 1;
+}
+</style>
