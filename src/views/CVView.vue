1195d66bd018df580b82323bcaef876238ab45c3
Corentin Hervaud <corentin.hervaud@epitech.eu>
2022-08-15 21:37:20 +0200

    improvment(cv): Change pdf to png #28

--- src/views/CVView.vue
+++ src/views/CVView.vue
@@ -1,40 +1,14 @@
 <template>
   <div class="CVContainer">
-    <h1 v-if="isLoading">Loading...</h1>
-    <br />
-    <pdfViewer :class="{ 'hide': isLoading }" class="CVItem" source="cv.pdf" @rendered="handleDocumentRender" />
+    <img src="cv.png" alt="CV" />
   </div>
 </template>
-
-<script>
-import pdfViewer from "vue-pdf-embed";
-
-export default {
-  components: {
-    pdfViewer,
-  },
-  data() {
-    return {
-      isLoading: true,
-    };
-  },
-  methods: {
-    funcy() {
-      console.log("Call funcy");
-    },
-    handleDocumentRender() {
-      this.isLoading = false;
-      console.log("rendered");
-    },
-  },
-};
-</script>
 
 <style scoped>
 .CVContainer {
   display: flex;
   flex: 1;
-  margin: 0 20% 0 20%;
+  justify-content: center;
 }
 .CVItem {
   flex: 1;
