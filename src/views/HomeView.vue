573b43f9effab7bf335de219eb1b3a9b68a7c03e
Corentin Hervaud <corentin.hervaud@epitech.eu>
2022-09-14 14:14:40 +0200

    feat(home): Add buttons to naviguate to cv and projects #34

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
   <AnimatedBackground />
   <div class="mainContainer">
-    <p class="mainText">
-      <span class="highViewText">Hello</span>, I'm a student <span class="moreViewText">developper</span> based on Bordeaux, France.
-      <br />
-      Welcome to my <span class="moreViewText">portfolio</span> !
-    </p>
-    <p class="title">Corentin<br/>Hervaud<span class="blinker">_</span></p>
+    <div class="textContainer">
+      <p class="mainText">
+        <span class="highViewText">Hello</span>, I'm a student <span class="moreViewText">developper</span> based on Bordeaux, France.
+        <br />
+        Welcome to my <span class="moreViewText">portfolio</span> !
+      </p>
+      <p class="title">Corentin<br/>Hervaud<span class="blinker">_</span></p>
+    </div>
+    <div class="buttonsContainer">
+      <router-link :to="{ name: 'cv' }">
+        <button class="cvButton">Consultez mon CV</button>
+      </router-link>
+      <router-link :to="{ name: 'projectsList' }">
+        <button class="projectsButton">Découvrir mes projets</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -25,7 +35,36 @@ export default {
   height: 100%;
   display: flex;
   flex: 1;
+  flex-direction: column;
+}
+.textContainer {
+  flex: 3;
+  display: flex;
   justify-content: space-around;
+}
+.buttonsContainer {
+  display: flex;
+  flex: 1;
+  align-items: flex-start;
+  justify-content: space-around;
+  margin-left: 25%;
+}
+.cvButton, .projectsButton {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 10px;
+  font-size: 23px;
+  cursor: pointer;
+}
+.cvButton {
+  background: #c25ee8;
+  border: 3px solid #8435a2;
+}
+.projectsButton {
+  background: #12c9a8;
+  border: 3px solid #10836f;
 }
 .mainText {
   margin: 0;
